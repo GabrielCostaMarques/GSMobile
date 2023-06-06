731cb8311d5c8390a54ebb2ef344a2b4ed3426d6
Pedro Dagostini <87324617+Dagostini01@users.noreply.github.com>
2023-06-06 19:53:37 -0300

    Aba Cadastrar feita

--- AwesomeProject/src/telas/Home/Home.js
+++ AwesomeProject/src/telas/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity, TextInput } from 'react-native';
 
 import Style from '../../../estilos/StyleHome';
 import Icon from '../../../assets/Icon.png';
@@ -12,74 +12,134 @@ import SobreNos from '../../../assets/SobreNos.png';
 const Tab = createBottomTabNavigator();
 
 export default function Home({ navigation }) {
-  const goDoar = () => {
-    navigation.navigate('Doar');
-  };
+    const goDoar = () => {
+        navigation.navigate('Doar');
+    };
 
-  const goSobre = () => {
-    navigation.navigate('SobreNos');
-  };
+    const goSobre = () => {
+        navigation.navigate('SobreNos');
+    };
 
-  return (
-    <NavigationContainer independent="true">
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Tela Inicial"
-          component={HomeScreen}
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <Image
-                source={Icon}
-                style={[
-                  Style.img,
-                  { tintColor: focused ? 'blue' : 'gray' },
-                ]}
-              />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer independent="true">
+            <Tab.Navigator>
+                <Tab.Screen
+                    name="Tela Inicial"
+                    component={HomeScreen}
+                    options={{
+                        tabBarIcon: ({ focused }) => (
+                            <Image
+                                source={Icon}
+                                style={[
+                                    Style.img,
+                                    { tintColor: focused ? 'blue' : 'gray' },
+                                ]}
+                            />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Cadastrar"
+                    component={CadastrarScreen}
+                    options={{
+                        tabBarIcon: ({ focused }) => (
+                            <Image
+                                source={Icon}
+                                style={[
+                                    Style.img,
+                                    { tintColor: focused ? 'blue' : 'gray' },
+                                ]}
+                            />
+                        ),
+                    }}
+                />
+            </Tab.Navigator>
+
+        </NavigationContainer>
+    );
 }
 
+function CadastrarScreen() {
+    const [nome, setNome] = React.useState('');
+    const [idade, setIdade] = React.useState('');
+
+    const handleNomeChange = (text) => {
+        setNome(text);
+    };
+
+    const handleIdadeChange = (text) => {
+        setIdade(text);
+    };
+
+    const handleSubmit = () => {
+        // Lógica para lidar com o envio do formulário
+        console.log('Nome:', nome);
+        console.log('Idade:', idade);
+    };
+
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={Style.titulo}>Cadastrar</Text>
+
+            <TextInput
+                style={Style.input}
+                placeholder="Nome"
+                value={nome}
+                onChangeText={handleNomeChange}
+            />
+
+            <TextInput
+                style={Style.input}
+                placeholder="Idade"
+                value={idade}
+                onChangeText={handleIdadeChange}
+            />
+
+            <TouchableOpacity style={Style.button} onPress={handleSubmit}>
+                <Text style={Style.buttonText}>Cadastrar</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+
 function HomeScreen() {
-  return (
-    <View style={{ flex: 1 }}>
-      <View style={Style.topo}>
-        <Text style={Style.titulo}>Olá Usuário</Text>
-        <Text style={Style.txt1}>São Paulo - SP</Text>
-        <Image source={Icon} style={Style.img} />
-      </View>
+    return (
+        <View style={{ flex: 1 }}>
+            <View style={Style.topo}>
+                <Text style={Style.titulo}>Olá Usuário</Text>
+                <Text style={Style.txt1}>São Paulo - SP</Text>
+                <Image source={Icon} style={Style.img} />
+            </View>
 
-      <View style={Style.menu}>
-        <Text style={Style.titulo1}>ATIVIDADES</Text>
+            <View style={Style.menu}>
+                <Text style={Style.titulo1}>ATIVIDADES</Text>
 
-        <TouchableOpacity style={Style.touchDoacoes}>
-          <Image source={Doacoes} style={Style.iconDoacoes}></Image>
-          <Text style={Style.txtIcon}>Doações</Text>
-        </TouchableOpacity>
+                <TouchableOpacity style={Style.touchDoacoes}>
+                    <Image source={Doacoes} style={Style.iconDoacoes}></Image>
+                    <Text style={Style.txtIcon}>Doações</Text>
+                </TouchableOpacity>
 
-        <TouchableOpacity
-          style={Style.touchDoar}
-          onPress={() => {
-            goDoar();
-          }}
-        >
-          <Image source={Doar} style={Style.iconDoar}></Image>
-          <Text style={Style.txtIcon}>Doar</Text>
-        </TouchableOpacity>
+                <TouchableOpacity
+                    style={Style.touchDoar}
+                    onPress={() => {
+                        goDoar();
+                    }}
+                >
+                    <Image source={Doar} style={Style.iconDoar}></Image>
+                    <Text style={Style.txtIcon}>Doar</Text>
+                </TouchableOpacity>
 
-        <TouchableOpacity
-          style={Style.touchSobre}
-          onPress={() => {
-            goSobre();
-          }}
-        >
-          <Image source={SobreNos} style={Style.iconSobre}></Image>
-          <Text style={Style.txtIcon}>Sobre Nós</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+                <TouchableOpacity
+                    style={Style.touchSobre}
+                    onPress={() => {
+                        goSobre();
+                    }}
+                >
+                    <Image source={SobreNos} style={Style.iconSobre}></Image>
+                    <Text style={Style.txtIcon}>Sobre Nós</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 }
