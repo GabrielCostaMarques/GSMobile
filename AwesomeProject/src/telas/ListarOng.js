6bff22ee1ac72cb41e4c553a93f4f8db6b70d155
matheus <matheusnagot@hotmail.com>
2023-06-07 13:39:51 -0300

    fix:att chamda api + useEffect & update dados lista

--- AwesomeProject/src/telas/ListarOng.js
+++ AwesomeProject/src/telas/ListarOng.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Text,
   View,
@@ -15,7 +15,7 @@ import axios from 'axios';
 import Style from '../../estilos/StyleSignIn';
 import Logo from '../../assets/images.png'
 
-const baseUrl = 'http://localhost:8080/api/v1';
+const baseUrl = 'http://192.168.0.4:8080/api/v1';
 
 export default function Register({ navigation }) {
   const api = axios.create({
@@ -25,44 +25,51 @@ export default function Register({ navigation }) {
     },
   });
 
+  useEffect(() => {
+    listar()
+  }, []);
+
+
   const goLogin = () => {
     navigation.navigate('Login');
   };
 
+  const [valor, setValor] = useState([])
+
   const listar = async () => {
     try {
-      const response = await axios.get(`${baseUrl}/ong/listar`)
-      alert(response.status);
+      const response = await axios.get(`${baseUrl}/ong/listarTodos`)
       setValor(response.data)
-      console.log(response.data)
     } catch (error) {
-      alert(error);
+      console.log("foobar", error);
     }
   };
 
-  const [valor, setValor] = useState([])
-
   const Item = (props) => {
-    console.log(props)
-    return(
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#7ed956', borderWidth: 2, borderColor: '#FFF', borderRadius: 20}}>
-        <Text style={{fontWeight: 'bold'}}>CNPJ: {props.dados.cnpj}</Text>
-        <Text style={{fontWeight: 'bold'}}>NOME: {props.dados.nome}</Text>
-        <Text style={{fontWeight: 'bold'}}>ENDEREÇO: {props.dados.endereco}</Text>
-        <Text style={{fontWeight: 'bold'}}>TELEFONE: {props.dados.telefone}</Text>
-        <Text style={{fontWeight: 'bold'}}>E-MAIL: {props.dados.email}</Text>
+    let endereco = props.dados.endereco
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#7ed956', borderWidth: 2, borderColor: '#FFF', borderRadius: 20 }}>
+        <Text style={{ fontWeight: 'bold' }}>CNPJ: {props.dados.cnpj}</Text>
+        <Text style={{ fontWeight: 'bold' }}>NOME: {props.dados.nome}</Text>
+        <Text style={{ fontWeight: 'bold' }}>LOGRADOURO: {endereco.logradouro}</Text>
+        <Text style={{ fontWeight: 'bold' }}>NUMERO: {endereco.numero}</Text>
+        <Text style={{ fontWeight: 'bold' }}>COMPLEMENTO: {endereco.complemento}</Text>
+        <Text style={{ fontWeight: 'bold' }}>CEP: {endereco.cep}</Text>
+        <Text style={{ fontWeight: 'bold' }}>UF: {endereco.uf}</Text>
+        <Text style={{ fontWeight: 'bold' }}>CIDADE: {endereco.cidade}</Text>
+        <Text style={{ fontWeight: 'bold' }}>TELEFONE: {props.dados.telefone}</Text>
+        <Text style={{ fontWeight: 'bold' }}>E-MAIL: {props.dados.email}</Text>
       </View>
     )
   }
 
   return (
     <ScrollView>
-      <View style={{flex: 1}}>
-      <Button title='listar' onPress={(listar)} />
-          <FlatList 
+      <View style={{ flex: 1 }}>
+        <FlatList
           data={valor}
-          renderItem={({item}) => <Item dados={item} />}
-          />
+          renderItem={({ item }) => <Item dados={item} />}
+        />
       </View>
     </ScrollView>
   );
