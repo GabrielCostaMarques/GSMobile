8f2d0b4db73bc0acd233ea4d3e0f2d74b48a7326
matheus <matheusnagot@hotmail.com>
2023-06-07 19:14:21 -0300

    fix: add api_url + limpeza & scrollview

--- AwesomeProject/src/telas/ListarOng.js
+++ AwesomeProject/src/telas/ListarOng.js
@@ -2,30 +2,19 @@ import React, { useState, useEffect } from 'react';
 import {
   Text,
   View,
-  TextInput,
-  TouchableOpacity,
-  Image,
-  StatusBar,
   ScrollView,
   FlatList,
-  Button
 } from 'react-native';
 
 import axios from 'axios';
-import Style from '../../estilos/StyleSignIn';
-import Logo from '../../assets/images.png'
-import { listarTodasOngs} from './api/api';
+import {API_URL} from './api'
 
 export default function Register() {
   const [valor, setValor] = useState([])
 
   useEffect(() => {
     listar()
-    
   }, []);
-
-  const API_URL = 'http://172.23.144.1:8080/api/v1';
-
 
 
   const listar = async () => {
@@ -41,6 +30,7 @@ export default function Register() {
   const Item = (props) => {
     let endereco = props.dados.endereco
     return (
+      <ScrollView>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#7ed956', borderWidth: 2, borderColor: '#FFF', borderRadius: 20 }}>
         <Text style={{ fontWeight: 'bold' }}>CNPJ: {props.dados.cnpj}</Text>
         <Text style={{ fontWeight: 'bold' }}>NOME: {props.dados.nome}</Text>
@@ -53,17 +43,16 @@ export default function Register() {
         <Text style={{ fontWeight: 'bold' }}>TELEFONE: {props.dados.telefone}</Text>
         <Text style={{ fontWeight: 'bold' }}>E-MAIL: {props.dados.email}</Text>
       </View>
+      </ScrollView>
     )
   }
 
   return (
-
       <View style={{ flex: 1 }}>
         <FlatList
           data={valor}
           renderItem={({ item }) => <Item dados={item} />}
         />
       </View>
-
   );
 }
