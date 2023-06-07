e2e3b9522216652d3d9df48fe2d52f9a4527bb2c
Pedro Dagostini <87324617+Dagostini01@users.noreply.github.com>
2023-06-06 23:47:36 -0300

    Aba listar back ok

--- AwesomeProject/src/telas/ListarOng.js
+++ AwesomeProject/src/telas/ListarOng.js
@@ -7,7 +7,8 @@ import {
   Image,
   StatusBar,
   ScrollView,
-  FlatList
+  FlatList,
+  Button
 } from 'react-native';
 
 import axios from 'axios';
@@ -30,19 +31,36 @@ export default function Register({ navigation }) {
 
   const listar = async () => {
     try {
-      const response = await axios.post(`${baseUrl}/ong/listar`)
+      const response = await axios.get(`${baseUrl}/ong/listar`)
       alert(response.status);
+      setValor(response.data)
+      console.log(response.data)
     } catch (error) {
       alert(error);
     }
   };
 
+  const [valor, setValor] = useState([])
+
+  const Item = (props) => {
+    console.log(props)
+    return(
+      <View>
+        <Text>{props.dados.cnpj}</Text>
+        <Text>{props.dados.nome}</Text>
+        <Text>{props.dados.endereco}</Text>
+      </View>
+    )
+  }
+
   return (
     <ScrollView>
       <View style={Style.container}>
           <FlatList 
-          data={}
+          data={valor}
+          renderItem={({item}) => <Item dados={item} />}
           />
+        <Button title='listar' onPress={(listar)} />
       </View>
     </ScrollView>
   );
